42f1852fd98a83e4061413a2e0d1470cade97eab
theshubz <157612314+theshubz@users.noreply.github.com>
2024-03-09 00:14:51 +0530

    update

--- PHP/logincheck.php
+++ PHP/logincheck.php
@@ -2,13 +2,6 @@
 	session_start();
 	if (isset($_SESSION['id'])) {
 		header('Location: ../index1.php');
-	}else{
-		// If the session is invalid, redirect the user to the home page
-		echo '<script type="text/javascript">'; 
-		echo 'alert("Invalid Session");'; 
-		echo 'window.location.href = "home.php";';
-		echo '</script>'; 
-		exit(); // Terminate further execution of the script
 	}
 
 
